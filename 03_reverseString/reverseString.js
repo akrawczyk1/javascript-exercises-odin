69d303e2b2eb76caebff70d966b0e820a6e0f54f
akrawczyk1 <71944688+akrawczyk1@users.noreply.github.com>
2025-03-03 12:45:47 -0500

    Solves exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    let returnString = "";
 
+    for (let i = s.length; i >= 0; i--){
+        returnString += s.charAt(i);
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
